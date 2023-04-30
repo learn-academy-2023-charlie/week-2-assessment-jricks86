3ed14e702092449bfd398154ed0c7c8e0bc5da26
jricks86 <jricks86@gmail.com>
2023-04-30 14:49:54 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,9 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+const { log } = require("console")
+
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -26,6 +30,13 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // Pseudo code:
+// input: object
+// output: String that identifies if the given number is divisible by three.
+// "15 is divisible by three"
+// "0 is divisible by three"
+// "-7 is not divisible by three"
+// create function div3
+// use if else conditional distinguish between if it is divisible or not
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -36,32 +47,113 @@ const object2 = { number: 0 }
 const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
+describe("div3", () => {
+    it("returns if the number inside it is evenly divisible by three or not", () => {
+        expect(div3(object1)).toEqual("15 is divisible by three")
+        expect(div3(object2)).toEqual("0 is divisible by three")
+        expect(div3(object3)).toEqual("-7 is not divisible by three")
+        
+    })
+})
+//  FAIL  ./code-challenges.test.js
+//   div3
+//   ✕ returns if the number inside it is evenly divisible by three or not
+// ● div3 › returns if the number inside it is evenly divisible by three or not
+//   ReferenceError: div3 is not defined
+
 // b) Create the function that makes the test pass.
+
+const div3 = (object) => {
+    if(object % 3 ===0) {
+        return `${object.number} is divisible by three`}
+    else {
+        return `${object.number} is not divisible by three`
+    }
+    }
+
+
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // Pseudo code:
+// input: array
+// output: array with all words capitalized
+// create a function called makeUppercase
+// iterate through the array using .map to return the same array length
+// use .slice to make the first letter of each word capitalized in addition to .toUpperCase
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+describe("makeUppercase", () => {
+    it("return an array with all the words capitalized", () => {
+    const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+//     // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+    const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+//     // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+
+    expect(makeUpercase(randomNouns1).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]))
+    expect(makeUpercase(randomNouns1).toEqual(["Temperature", "Database", "Chopsticks", "Mango"]))
+    })
+})
+// FAIL  ./code-challenges.test.js
+// makeUppercase
+//   ✕ return an array with all the words capitalized (1 ms)
+// ● makeUppercase › return an array with all the words capitalized
+//   ReferenceError: makeUpercase is not defined
 
 // b) Create the function that makes the test pass.
+
+const makeUppercase = (words) => {
+    return words.map(word => word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase())
+}
+
+//  PASS  ./code-challenges.test.js
+//   makeUppercase
+//   ✓ return an array with all the words capitalized (1 ms)
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // Pseudo code:
+// input: string
+// output: index of the first vowel
+// create a function firstVowel
+// use for loop to iterate through the string and .includes to only look for the vowels
+// use [i] with the given strings to output index numbers instead of the values
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const vowelTester1 = "learn"
-// Expected output: 1
-const vowelTester2 = "academy"
-// Expected output: 0
-const vowelTester3 = "challenges"
-// Expected output: 2
+describe("firstVowel", () => {
+    it("logs the index of the first vowel", () => {
+        const vowelTester1 = "learn"
+        // Expected output: 1
+        const vowelTester2 = "academy"
+        // Expected output: 0
+        const vowelTester3 = "challenges"
+        // Expected output: 2        
+        expect(firstVowel(vowelTester1)).toEqual(1)
+        expect(firstVowel(vowelTester2)).toEqual(0)
+        expect(firstVowel(vowelTester3)).toEqual(2)
+    })
+})
+// FAIL  ./code-challenges.test.js
+// makeUppercase
+//   ✓ return an array with all the words capitalized (1 ms)
+// firstVowel
+//   ✕ logs the index of the first vowel
+// ● firstVowel › logs the index of the first vowel
+//   ReferenceError: firstVowel is not defined
 
 // b) Create the function that makes the test pass.
+const vowels = ["a","e", "i", "o", "u"]
+const firstVowel = (string) => {
+    for (let i=0; i < string.length; i++) {
+        if (vowels.includes(string[i])) {
+            return i
+        }
+    }
+}
+// PASS  ./code-challenges.test.js
+// makeUppercase
+//   ✓ return an array with all the words capitalized (1 ms)
+// firstVowel
+//   ✓ logs the index of the first vowel (1 ms)
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,50 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: A parameter is a placeholder when you define a function to let the function know what type of input it is receiving. An argument is the actual value being used in the parameter when you do a function call.
 
 Researched answer:
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer: Value, index, and array.  The value is required and the other two are optional
 
-Researched answer:
+Researched answer: The value parameter contains the value of the current element of the array. Using map implements an action to each value of the array, so the value parameter is required.
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer: map gives an output of an array with the same length and filter gives an output of selective elements inside the array 
 
-Researched answer:
+Researched answer: Both are higher order functions used to manipulate arrays. Using map mutates each element in the array and outputs an array of the new elements. In a filter, a callback function is used for each element with a result being true or false.  Only true elements are returned in the output.
 
-1. What is iteration?
+4. What is iteration?
 
-Your answer:
+Your answer: Iteration does something over and over until a specified result is met
 
-Researched answer:
+Researched answer: Iteration involves using loops to execute a block of code multiple times, either until a certain condition is met or for a specified number of iterations
 
-1. What is the difference between a function and a method?
+5. What is the difference between a function and a method?
 
-Your answer:
+Your answer: a function takes in parameters and tells it to return a result. Methods are behaviors that are applied to objects.
 
-Researched answer:
+Researched answer: => are not used with methods
 
 6. STRETCH: What is hoisting in JavaScript?
 
-Your answer:
+Your answer: Though I am not familiar with hoisting in Javascript, just the word itself tells me that it would involve moving something higher. Possibly moving code to a higher position.
 
-Researched answer:
+Researched answer: Regardless of where variables and functions are declared in the code, they are moved to the top of their scope, which allows them to be used before they are declared in the code.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Component:
+1. Component: Returns data to the user interface through JSX.
 
-2. Spread operator:
+2. Spread operator: Allows us to expand or spread out an iterable such as an array or an object into individual elements.
 
-3. React state:
+3. React state: Built-in feature of React that allows you to store and manage component-level data that can change over time.
 
-4. React props:
+4. React props: Properties that provide a way for components to communicate with each other, share information and control the behavior of child components.
 
-5. DOM events:
+5. DOM events: Actions or occurrences that happen in the Document Object Model (DOM). Examples include clicking a button, typing a key on the keyboard, scrolling the page, and resizing the window. 
